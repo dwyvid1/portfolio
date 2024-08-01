f26fc2838216ca45dca4e3e4febf3e9d1b846e4f
Deyvid Matheus <deyvid.nascimento0101@gmail.com>
2024-07-31 21:56:24 -0300

    Adjusting project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './header.scss'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div>
+                <h1>Olá, eu sou o Deyvid</h1>
+                <h2>Estudante de TI</h2>
+            </div>
+
+            <img src="https://github.com/dwyvid1.png"/>
+        </div>
+    )
+}
