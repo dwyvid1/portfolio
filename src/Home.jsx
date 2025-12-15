8a37dffb84736155ea6c47a0e1d3d3b943dba30d
Deyvid Matheus <deyvid.nascimento0101@gmail.com>
2025-12-15 13:17:21 -0300

    Tecnologias

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import Main from "./components/Main"
+import Footer from "./components/Footer";
 
 function Home() {
 
@@ -7,6 +8,7 @@ function Home() {
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   )
 }
